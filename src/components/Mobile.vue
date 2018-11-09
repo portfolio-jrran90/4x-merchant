40e10cd94c2384071a77874415fe307c29c55670
Jhon Rey Ranario <jr.ran90@gmail.com>
2018-11-09 17:31:35 +0800

    update

--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 offset-md-3">
-        <div class="card mt-5">
+        <div class="card mt-3">
           <div class="card-header">
             <h2 class="mb-0">Merchant - Ilbisonte</h2>
           </div>
@@ -27,11 +27,38 @@
         <div class="card mt-3">
           <div class="card-body">
             <h4 class="mb-3">MASUKKAN NO TELP UNTUK APPROVAL PEMBAYARAN</h4>
-            <input type="text" class="form-control" placeholder="PHONE NUMBER">
-            <button class="btn btn-primary btn-block mt-2">SEND</button>
+            <input type="text" class="form-control" placeholder="PHONE NUMBER" v-model="hp">
+            <button class="btn btn-primary btn-block mt-2" @click.prevent="send" :disabled="!hp">SEND</button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      hp: ''
+    }
+  },
+  methods: {
+    send() {
+      let vm = this
+      let data = {
+        duit: 3500000,
+        m: '081808314197',
+        p: 'ILB',
+        hp: vm.hp
+      }
+      axios.post('https://sandbox2.empatkali.co.id/initiatepurchase', data).then(res => {
+        alert('Successfully Sent!')
+        vm.hp = ''
+      })
+    }
+  }
+}
+</script>
