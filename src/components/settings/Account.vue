aefbd2546bbe43981785a516527421e71189af19
Jhon Rey Ranario <jr.ran90@gmail.com>
2019-02-22 16:14:03 +0800

    update

--- src/components/settings/Account.vue
+++ src/components/settings/Account.vue
@@ -7,35 +7,38 @@
 		  </ol>
 		</nav>
     <div class="row">
+      <div class="col-md-3">
+        <img :src="merchantDetail.image" class="img-thumbnail" style="width: 250px; height: 250px; object-fit: cover" alt="">
+      </div>
     	<div class="col">
     		<table class="table table-bordered">
     			<tr>
-    				<td class="w-25 table-info">Id Merchant</td>
-    				<td>1234</td>
+    				<td class="table-info" style="width: 35%">Id Merchant</td>
+    				<td>{{ merchantDetail._id }}</td>
     			</tr>
     			<tr>
     				<td class="table-info">Nama Merchant</td>
-    				<td>1234</td>
+    				<td>{{ merchantDetail.name }}</td>
     			</tr>
     			<tr>
     				<td class="table-info">Nama Penanggung Jawab</td>
-    				<td>1234</td>
+    				<td>{{ merchantDetail.username }}</td>
     			</tr>
     			<tr>
     				<td class="table-info">No. Akta Perusahaan</td>
-    				<td>1234</td>
+    				<td>---</td>
     			</tr>
     			<tr>
     				<td class="table-info">Alamat</td>
-    				<td>1234</td>
+    				<td>---</td>
     			</tr>
     			<tr>
     				<td class="table-info">Nomor Telepon</td>
-    				<td>1234</td>
+    				<td>---</td>
     			</tr>
     			<tr>
     				<td class="table-info">Alamat Email</td>
-    				<td>1234</td>
+    				<td>{{ merchantDetail.email }}</td>
     			</tr>
     		</table>
     	</div>
@@ -44,7 +47,24 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      merchantDetail: {}
+    }
+  },
+  created() {
+    let vm = this
+    axios.get(`${process.env.VUE_APP_API_URL}/api/merchants/detail`, {
+      headers: {
+        'Authorization': process.env.VUE_APP_AUTHORIZATION,
+        'x-access-token': localStorage.getItem('auth_token')
+      }
+    }).then(res => vm.merchantDetail = res.data)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
