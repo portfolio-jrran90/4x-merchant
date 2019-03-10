a08242a6ecd0d1ec2271b0c979b42fc52aec92b9
dhonaldJC <dhonaldjohannes@gmail.com>
2019-03-10 16:55:52 +0700

    update transaction page

--- src/components/transactions/Index.vue
+++ src/components/transactions/Index.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="py-4">
     <h2>
-      <font-awesome-icon icon="exchange-alt"/> Transaksi
+      <font-awesome-icon icon="exchange-alt"/>Transaksi
     </h2>
     <div class="card">
       <div class="card-header">
         <div class="form-inline">
           <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Filter by:</label>
-          <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref" v-model="filterQuery">
+          <select
+            class="custom-select my-1 mr-sm-2"
+            id="inlineFormCustomSelectPref"
+            v-model="filterQuery"
+          >
             <option value="date">date</option>
             <option value="store">store</option>
           </select>
-          
+
           <!-- date range -->
           <date-range-picker
             :opens="filterDateRange.opens"
@@ -19,58 +23,84 @@
             :endDate="filterDateRange.endDate"
             @update="updateValues"
             :locale-data="{ firstDay: 1, format: 'DD-MM-YYYY' }"
-            :minDate="filterDateRange.minDate" :maxDate="filterDateRange.maxDate"
+            :minDate="filterDateRange.minDate"
+            :maxDate="filterDateRange.maxDate"
             class="mr-2"
             v-if="filterQuery==='date'"
           >
-            <div slot="input" slot-scope="picker">
-              {{ picker.startDate | date }} - {{ picker.endDate | date }}
-            </div>
+            <div
+              slot="input"
+              slot-scope="picker"
+            >{{ picker.startDate | date }} - {{ picker.endDate | date }}</div>
           </date-range-picker>
 
           <!-- store -->
           <select class="form-control mr-2" v-if="filterQuery==='store'">
-            <option value="">-- select</option>
+            <option value>-- select</option>
             <option :value="store._id" v-for="store in stores">{{ store.name }}</option>
           </select>
-
+          
           <button type="button" class="btn btn-primary my-1" @click="filterResult">Filter Result</button>
         </div>
-        
-        <div class="alert alert-info mb-0 mt-3">
-          <h5 class="mb-0">Result(s):</h5>
-          Total rows: {{ transactions.length }}
-        </div>
 
+        <div
+          class="alert alert-info mb-0 mt-3"
+        >Merchant anda memiliki {{ transactions.length }} transaksi</div>
       </div>
       <div class="card-body">
-        <table class="table table-striped table-bordered mb-0 table-sm" style="border: 0 !important">
+        <table
+          class="table table-striped table-bordered mb-0 table-sm"
+          style="border: 0 !important"
+        >
           <thead class="table-dark">
             <tr>
+              <th rowspan="2" class="text-center">Date</th>
+              <th rowspan="2" class="text-center">Time</th>
+              <th rowspan="2" class="text-center">Customer Identity</th>
               <th rowspan="2" class="text-center">No. Transaksi</th>
-              <th rowspan="2" class="text-center">Date Time</th>
-              <th colspan="2" class="text-center">Amount</th>
-              <th colspan="2" class="text-center">Payment Status</th>
+              <th rowspan="2" class="text-center">Gross Sales</th>
+              <th rowspan="2" class="text-center">Net Sales</th>
+
+              <th colspan="2" class="text-center">Settlement Confirmation</th>
+              <th colspan="3" class="text-center">Payment Status</th>
             </tr>
             <tr>
-              <th class="text-center">Gross</th>
-              <th class="text-center">Net</th>
+              <th class="text-center">Transfer</th>
+              <th class="text-center">Date</th>
               <th class="text-center">Approve by EMPATKALI</th>
               <th class="text-center">Payment Confirm</th>
+              <th class="text-center">Date</th>
             </tr>
           </thead>
-          <tbody v-if="transactions.length===0">
-            <tr><td colspan="6">No transaction(s)</td></tr>
+          <tbody v-if="transactions.length === 0">
+            <tr>
+              <td colspan="11">No transaction(s)</td>
+            </tr>
           </tbody>
           <tbody v-else>
             <tr v-for="data in transactions">
-              <td class="text-center">{{ data.transactionNumber }}</td>
+              <td class="text-center">{{ data.tgl }}</td>
               <!-- <td class="text-right">{{ Intl.NumberFormat('id-ID', {style: 'currency', currency: 'IDR'}).format(data.total) }}</td> -->
-              <td class="text-center">2012-12-12</td>
-              <td class="text-right">Rp. 1.000.000</td>
-              <td class="text-right">Rp. 1.000.000</td>
+              <td class="text-center">{{ data.jam }}</td>
+              <td class="text-right">{{ data.cust_identity }}</td>
+              <td class="text-right">{{ data.invoice }}</td>
+              <td class="text-center">{{ data.jumlah}}</td>
+              <td class="text-center">{{ (data.jumlah)*'10%'}}</td>
+              <td class="text-right">{{ data.mid }}</td>
+              <td class="text-right">{{ data.paymentdate }}</td>
               <td class="text-center">Approve by EMPATKALI</td>
-              <td class="text-center">Payment Confirm</td>
+              <td class="text-center">
+                <small v-if="data.status==0" style="color: orange">
+                  <b>Pending</b>
+                </small>
+                <small v-if="data.status==1" style="color: darkblue">
+                  <b>On-Going</b>
+                </small>
+                <small v-if="data.status==2" style="color: green">
+                  <b>Confirm</b>
+                </small>
+              </td>
+              <td class="text-center">{{ data.paymentdate }}</td>
             </tr>
           </tbody>
         </table>
@@ -127,50 +157,46 @@
 
 <script>
 import axios from "axios";
-import DateRangePicker from 'vue2-daterange-picker'
-import moment from 'moment'
+import DateRangePicker from "vue2-daterange-picker";
+import moment from "moment";
 
 export default {
   components: {
-    DateRangePicker,
+    DateRangePicker
   },
   data() {
     return {
       // Search
-      filterQuery: 'date',
+      filterQuery: "date",
       filterDateRange: {
-        opens: 'center',
+        opens: "center",
         startDate: new Date(),
         endDate: new Date(),
-        minDate: '2017-09-02',
-        maxDate: '2020-10-02'
+        minDate: "2017-09-02",
+        maxDate: "2020-10-02"
       },
 
       stores: {},
 
       modalShowTransactionDetail: false,
       transactions: {},
-      transactionDetails: {},
+      transactionDetails: {}
     };
   },
   created() {
-    let vm = this
+    let vm = this;
 
-    axios.get(`${process.env.VUE_APP_API_URL}/api/approvedtransactions/merchant`, {
-      headers: {
-        'Authorization': process.env.VUE_APP_AUTHORIZATION,
-        'x-access-token': localStorage.getItem('auth_token')
-      }
-    }).then(res => vm.transactions = res.data)
-
-    axios.get(`${process.env.VUE_APP_API_URL}/api/stores`, {
-      headers: {
-        'Authorization': process.env.VUE_APP_AUTHORIZATION,
-        'x-access-token': localStorage.getItem('auth_token')
-      }
-    }).then(res => vm.stores = res.data)
-
-    // api/stores?limit=100&skip=0
+    axios
+      .get(
+        `http://sandbox.empatkali.co.id/transaksi.php?id=5c63b4b88b904e1c13672183&d=2019-02-22&k=2019-03-25&m=c00bdb56c3e31c0761a91b4fc79fa530`,
+        {
+          headers: {
+            Authorization: process.env.VUE_APP_AUTHORIZATION,
+            "x-access-token": localStorage.getItem("auth_token")
+          }
+        }
+      )
+      .then(res => (vm.transactions = res.data));
   },
   methods: {
     openModal(modal, data) {
@@ -181,25 +207,30 @@ export default {
           vm.transactionDetails = {};
           vm.modalShowTransactionDetail = true;
           axios
-            .get(`${process.env.VUE_APP_API_URL}/merchanttransactions/${data.No}`)
+            .get(
+              `${process.env.VUE_APP_API_URL}/merchanttransactions/${data.No}`
+            )
             .then(res => {
               vm.transactionDetails = Object.assign(res.data, {
                 totals: data.Total
               });
             });
           break;
-        default: alert("error!");
+        default:
+          alert("error!");
       }
     },
 
-    updateValues (values) {
-      console.log('oh', values)
-      this.filterDateRange.startDate = values.startDate.toISOString().slice(0, 10)
-      this.filterDateRange.endDate = values.endDate.toISOString().slice(0, 10)
+    updateValues(values) {
+      console.log("oh", values);
+      this.filterDateRange.startDate = values.startDate
+        .toISOString()
+        .slice(0, 10);
+      this.filterDateRange.endDate = values.endDate.toISOString().slice(0, 10);
     },
 
     filterResult() {
-      alert()
+      alert();
     }
   }
 };
